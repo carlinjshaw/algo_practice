3c95f6610d2bab5b5169fd14d874866584dd6f5c
carlinjshaw <carlinjshaw@gmail.com>
2023-01-20 12:12:21 -0500

    quick attempt; recursive permutation tree

---
+++ arrays/TwoStringPermutation.js
@@ -0,0 +1,22 @@
+const a = "car"
+const b = "rac"
+
+const isPermutation = (a, b) => {
+    //make permutations and push them to an array
+    //make a permutation,
+    //recursive 
+    //you have ten positions
+    //for each position, swap that for the first 
+    if(a == b){return true}
+
+    
+}
+let perms = [];
+
+const switchLetters = (str, index) => {
+    if(index ==0) {perms.push(str)}
+
+
+}
+
+console.log(isPermutation(a,b))
